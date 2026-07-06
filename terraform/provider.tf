f8b95024b3db50fac02da7390ee5c3c3629983d4
fatmanunk <qahorton@gmail.com>
2026-07-06 00:21:15 -0400

    add terraform CI/CD workflow and remote state backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,11 +9,18 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "quentonhorton-crc-tfstate"
+    key            = "cloud-resume/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "cloud-resume-tflock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "terraform"
+  region = "us-east-1"
 
   default_tags {
     tags = {
@@ -23,4 +30,3 @@ provider "aws" {
     }
   }
 }
-
